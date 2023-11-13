6f08c18cfef922b608bb9039cd058885d199bd16
Hannes Alexandersson <135141396+HannesAlexandersson@users.noreply.github.com>
2023-11-13 12:03:06 +0100

    grid

---
+++ grid/02.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Grid 02</title>
+    <link rel="stylesheet" href="grid-styling.css">
+</head>
+<body>
+    <div class="grid-wrapper">
+        <div class="grid-item one">1</div>
+        <div class="grid-item two">2</div>
+        <div class="grid-item three">3</div>
+        <div class="grid-item four">4</div>
+        <div class="grid-item five">5</div>
+    </div>
+</body>
+</html>
---
+++ grid/04.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Grid 02</title>
+    <link rel="stylesheet" href="styling04.css">
+</head>
+<body>
+    <div class="grid-wrapper">
+        <div class="grid-item one black">1</div>
+        <div class="grid-item two black">2</div>
+        <div class="grid-item three black">3</div>
+        <div class="grid-item four black">4</div>
+        <div class="grid-item five green">5</div>   
+        <div class="grid-item six green">6</div>    
+        <div class="grid-item seven green">7</div>
+        <div class="grid-item eight green">8</div>
+        <div class="grid-item nine green">9</div>
+        <div class="grid-item ten green">10</div>
+        <div class="grid-item eleven green">11</div>
+        <div class="grid-item twelve green">12</div>
+        <div class="grid-item thirteen green">13</div>
+        <div class="grid-item fourteen green">14</div>
+        <div class="grid-item fifteen black">15</div>
+        <div class="grid-item sixteen green">16</div>
+        <div class="grid-item seventeen green">17</div>
+        <div class="grid-item eighteen green">18</div>
+        <div class="grid-item nineteen green">19</div>
+        <div class="grid-item twenty black">20</div>
+        <div class="grid-item twenty-one black">21</div>
+        <div class="grid-item twenty-two black">22</div>
+
+    </div>
+</body>
+</html>
+
+<?php
+    /* for ( $i = 1; $i < 150; $i++ ) {?>
+       <div class="grid-item <?=$i?>"><?=$i?></div><?php
+    } */?>
